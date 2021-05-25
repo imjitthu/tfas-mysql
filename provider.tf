3d71fda44072224884a179fe95937ab3878f5726
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-25 09:47:53 +0530

    roboshop

--- provider.tf
+++ provider.tf
@@ -2,12 +2,3 @@ provider "aws" {
   profile = "terraform"
   region  = "us-east-1"
 }
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.21"
-    }
-  }
-}
