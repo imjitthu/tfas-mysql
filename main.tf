8490c0f139dc090ee94cf01f485e68e3b4b4ab9b
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-25 09:20:48 +0530

    roboshop

--- main.tf
+++ main.tf
@@ -1,41 +1,81 @@
-locals {
-  key_name = "test"
-  key_path = "test.pem"
+resource "aws_db_subnet_group" "mysql" {
+  name       = "mysql"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
+  tags       = {
+    name     = "Mysql DB subnet grp"
+  }
 }
 
-resource "aws_instance" "mysql" {
-  # aws_spot_instabce_request for spot instance
-  ami = data.aws_ami.ami.id
-  instance_type = "${var.INSTANCE_TYPE}"
-  key_name = local.key_name
-  # spot_type = "one-time"  aws_spot_instance_request
-  tags = {
-    "Name" = "${var.COMPONENT}-Server"
+resource "aws_rds_cluster_parameter_group" "mysql" {
+  name   = "mysql-cluster-pg-${var.ENV}"
+  family = "aurora-mysql5.7"
+  description = "RDS default cluster parameter group"
+}
+
+resource "aws_rds_cluster" "mysql" {
+  cluster_identifier              = "mysql-${var.ENV}"
+  engine                          = "aurora-mysql"
+  engine_version                  = "5.7.mysql_aurora.2.03.2"
+  db_subnet_group_name            = aws_db_subnet_group.mysql.name
+  database_name                   = "defaultdb"
+  master_username                 = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["MYSQL_USER"]
+  master_password                 = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["MYSQL_PASS"]
+  backup_retention_period         = 5
+  preferred_backup_window         = "07:00-09:00"
+  skip_final_snapshot             = true
+  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.mysql.name
+  vpc_security_group_ids          = [aws_security_group.allow_mysql.id]
+}
+
+resource "aws_rds_cluster_instance" "cluster_instances" {
+  count              = 1
+  identifier         = "mysql-${var.ENV}-${count.index}"
+  cluster_identifier = aws_rds_cluster.mysql.id
+  instance_class     = "db.t3.small"
+  engine             = aws_rds_cluster.mysql.engine
+  engine_version     = aws_rds_cluster.mysql.engine_version
+}
+
+
+resource "aws_security_group" "allow_mysql" {
+  name          = "allow-mysql-${var.ENV}"
+  description   = "allow-mysql-${var.ENV}"
+  vpc_id        = data.terraform_remote_state.vpc.outputs.VPC_ID
+  ingress {
+    description = "SSH"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR,data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
 
- connection {
-  host = aws_instance.mysql.public_ip
-  type = "ssh"
-  #private_key = file("${local.key_path}") read private_key (PEM) from file
-  user = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["SSH_USER"]
-  password = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["SSH_PASS"]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags          = {
+    Name        = "allow-mysql-${var.ENV}"
+  }
 }
 
-provisioner "remote-exec" {
-  inline = [ "set-hostname ${var.COMPONENT}" ]
-}
-
-provisioner "local-exec" {
-  command = "echo ${aws_instance.mysql.public_ip} > mysql_inv"
-  #command = "ansible-playbook -i ${aws_instance.mysql.public_ip}, --private-key ${local.key_path} ${var.COMPONENT}.yml"
-  #echo $IP component=${component} ansible_user=root ansible_password=DevOps321 >>inv
-}
+resource "null_resource" "mysql-schema" {
+  provisioner "local-exec" {
+    command     = <<EOF
+    sleep 600
+    rm -rf rs-mysql
+    git clone https://github.com/SrimaanPenugonda/rs-mysql.git
+    cd rs-mysql
+    mysql -h ${aws_rds_cluster.mysql.endpoint} -u${jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["MYSQL_USER"]} -p${jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["MYSQL_PASS"]} <shipping.sql
+  EOF
+  }
 }
 
 resource "aws_route53_record" "mysql" {
-  zone_id = "${var.R53_ZONE_ID}"
-  name = "${var.COMPONENT}.${var.DOMAIN}"
-  type = "A"
-  ttl = "300"
-  records = [ aws_instance.mysql.public_ip ]
-}
+  name          = "mysql-${var.ENV}"
+  type          = "CNAME"
+  ttl           = "1000"
+  zone_id       = data.terraform_remote_state.vpc.outputs.ZONE_ID
+  records       = [aws_rds_cluster.mysql.endpoint]
