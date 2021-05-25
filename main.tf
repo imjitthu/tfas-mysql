1cac973745c749f6858d509da198fed6a3e163d7
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-25 10:55:52 +0530

    roboshop

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "null_resource" "import-mysql-schema" {
 
 data "aws_route53_zone" "jithendar" {
   name         = "jithendar.com"
-  private_zone = true
+  private_zone = false
 }
 
 resource "aws_route53_record" "jithendar" {
